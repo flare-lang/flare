ea4f8405a48ac7e21da97f06c993bdab5240eaee
Alex Rønne Petersen <alex@alexrp.com>
2020-09-09 13:31:59 +0200

    Use <InternalsVisibleTo /> to expose stuff to the tests project.

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -8,8 +8,6 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Flare.Cli.Commands;
 
-[assembly: InternalsVisibleTo("flare-tests.TestAdapter")]
-
 namespace Flare.Cli
 {
     static class Program
