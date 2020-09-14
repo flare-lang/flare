d474280b475942dee146548ed25eb27da8d6a77f
Alex Rønne Petersen <alex@alexrp.com>
2020-09-14 03:52:52 +0200

    Avoid async/await in the benchmark runner.

--- src/bench/Program.cs
+++ src/bench/Program.cs
@@ -2,14 +2,13 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
 using System.Reflection;
-using System.Threading.Tasks;
 using BenchmarkDotNet.Running;
 
 namespace Flare.Benchmarks
 {
     static class Program
     {
-        static Task<int> Main(string[] args)
+        static int Main(string[] args)
         {
             var root = new RootCommand()
             {
@@ -24,7 +23,7 @@ namespace Flare.Benchmarks
             root.Handler = CommandHandler.Create<BenchmarkOptions>(opts =>
                 _ = BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).RunAll(new BenchmarkConfig(opts)));
 
-            return root.InvokeAsync(args);
+            return root.Invoke(args);
         }
     }
 }
