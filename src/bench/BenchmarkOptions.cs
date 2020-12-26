77bbcbb3042d018113e1e047168130e954250ba8
Alex Rønne Petersen <alex@alexrp.com>
2020-12-26 10:02:12 +0100

    Fix CLI option parsing in the benchmark runner.
    This seems to have been a breaking change in System.CommandLine.

--- src/bench/BenchmarkOptions.cs
+++ src/bench/BenchmarkOptions.cs
@@ -2,10 +2,10 @@ namespace Flare.Benchmarks
 {
     sealed class BenchmarkOptions
     {
-        public string? Filter { get; private set; }
+        public string? Filter { get; set; }
 
-        public bool Test { get; private set; }
+        public bool Test { get; set; }
 
-        public bool Export { get; private set; }
+        public bool Export { get; set; }
     }
 }
